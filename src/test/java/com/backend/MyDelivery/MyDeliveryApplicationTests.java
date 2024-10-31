7161d50bcc6595c168e73dad8553db9fda5f4d4a
YeZawHlaing <yezawhlaing0@gmail.com>
2024-10-31 19:58:44 +0630

    initial commit

---
+++ src/test/java/com/backend/MyDelivery/MyDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.MyDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
